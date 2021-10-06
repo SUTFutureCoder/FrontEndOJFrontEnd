d4d2c8f29974e4d60a9013d8595b07b2622e1ece
XingChen Lin <FutureCoder@aliyun.com>
2021-10-06 15:42:25 +0800

    update cdn

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,3 @@
 export const BASE_BACKEND = 'http://localhost:8086'
 
-export const MONACO_CDN = 'https://cdn.bootcdn.net/ajax/libs/monaco-editor/0.13.1/min/vs'
+export const MONACO_CDN = 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.13.1/min/vs'
